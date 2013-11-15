e2baa855ac876bb86637ae15e780a0b8e85783b4
Pier-Olivier Thibault <pothibo@gmail.com>
2013-11-15 12:06:51 -0500

    Modified route because of WordPress. So, Wordpress has decided to not use restful resources. So I'm stuck creating a ugly monster of a route and change the post_path to reflect that awful scheme. Don't want to loose my precious google ranking, you know.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 PothiboCom::Application.routes.draw do
   root 'posts#index'
 
-  resources :posts, only: [:show, :index]
+  get '/:year/:month/:id', to: 'posts#show', constraints: { year: /\d+/, month: /\d+/ }, as: "post"
+
+  resources :posts, only: [:index]
 
   resource :session, only: [:create, :destroy]
 
