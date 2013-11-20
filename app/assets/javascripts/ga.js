0f7cddaedfc747a37fb3c3876cc3509e3a719cbd
Pier-Olivier Thibault <pothibo@gmail.com>
2013-11-20 09:10:16 -0500

    Moved google analytics to it's own asset file. Modified bouncing rules. A scrolling event has been added to the tracker so bouncing is measured by those that just land on the page and leave without reading. Makes more sense since the goal is to make people read. Also, A/B Testing will be much more useful to measure bouncing.

---
+++ app/assets/javascripts/ga.js
@@ -0,0 +1,21 @@
+(function() {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-16312790-3']);
+  _gaq.push(['_setDomainName', 'pothibo.com']);
+  _gaq.push(['_trackPageview']);
+
+  setTimeout(function() {
+    var unbounce = function() {
+      document.removeEventListener('scroll', unbounce)
+      _gaq.push(['_trackEvent', 'scroll', 'read']);
+    }
+
+    document.addEventListener('scroll', unbounce) 
+  }, 5000);
+
+  (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+})()
