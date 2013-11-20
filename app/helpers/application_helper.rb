ed881d335ad120cb98f1d3cf64918aef2f24e1f3
Pier-Olivier Thibault <pothibo@gmail.com>
2013-11-20 09:43:45 -0500

    Modified admin navigation bar so it's less of an eye sore.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def admin_access_bar
+    content_tag :div, id: "adminAccessBar" do
+      [
+        link_to(t(".admin"), admin.root_path, class: %w(admin link)),
+        button_to(t(".logout"), session_path, method: :delete)
+      ].join.html_safe
+    end
+  end
+
   def title
     return @post.title unless @post.nil?
     return "pothibo's blog" 
