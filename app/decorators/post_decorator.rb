64cd96c2704f7fe32d78cbd7ec632889c771206a
Pier-Olivier Thibault <pothibo@gmail.com>
2013-11-20 19:34:19 -0500

    Modified some styling and adding more information for suggested posts

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -1,7 +1,16 @@
 class PostDecorator < Cubisme::Decorator::Base
   def suggested(options)
     content_tag :li do
-      link_to record.title, post_path(year: record.published_at.year, month: l(record.published_at, format: "%m"), id: record.slug, bouncing: true, only_path: false)
+      content = [
+        link_to(record.title,
+                post_path(year: record.published_at.year,
+                          month: l(record.published_at, format: "%m"),
+                          id: record.slug, bouncing: true, only_path: false))
+      ]
+      if options[:excerpt] == true
+        content << content_tag(:p, record.excerpt, class: %w(excerpt))
+      end
+      content.join.html_safe
     end
   end
 end
