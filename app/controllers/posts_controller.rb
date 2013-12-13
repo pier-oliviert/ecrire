f32a5de6b72b0e8455fe4885558dea5d24aa0679
Pier-Olivier Thibault <pothibo@gmail.com>
2013-12-13 08:41:07 -0500

    Removed AB testing. Not yet ready

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,6 @@ class PostsController < ApplicationController
     @post = Post.find_by_slug(params[:id])
     redirect_to :root and return if @post.nil?
     redirect_to :root and return unless @post.published?
-    result = ab_test "social_sharing_location", "share_top", "share_bottom"
-
-    render "posts/splits/show/#{result}"
   end
 
   protected
