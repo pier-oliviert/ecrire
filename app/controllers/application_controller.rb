692022271ab3c060a8f3551925469b8021027bec
Pier-Olivier Thibault <pothibo@gmail.com>
2013-12-27 15:18:46 -0500

    Admin menu overhaul
    I've rebuild the whole admin menu to use a new algorithm I've been using
    in La Marmite. I think it gives a bit of balance between convention and
    customization.
    
    On the other hand, I feel the callbacks used are a bit cumbersome. Some
    of them could be actually remove so it's easier to implement custom
    state without bring the 7 hells of the callbacks into play. I guess it
    will be good for the time being but I'll keep a close eye on it. If I
    follow up with my idea of building a gem out of this concept, I'll fix
    it there.
    
    The text is a bit blurry on antialiasing but I think it's a nice
    incremental change from what it was before.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,36 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :signed_in?
   
+  helper_method :menu
+
+  def menu
+    @menu ||= Admin::MenuHelper::Menu.new request do |menu|
+      menu.add :blog do |item|
+        item.path = root_path
+        item.label = I18n.t('admin.navigation.blog')
+        item.highlight_when do |request|
+          !request.path.start_with?('/admin')
+        end
+      end
+
+      menu.add :posts do |item|
+        item.path = admin_posts_path
+        item.label = I18n.t('admin.navigation.posts')
+        item.highlight_when do |request|
+          request.env['action_controller.instance'].kind_of?(Admin::PostsController)
+        end
+      end
+
+      menu.add :partials do |item|
+        item.path = admin_partials_path
+        item.label = I18n.t('admin.navigation.partials')
+        item.highlight_when do |request|
+          request.env['action_controller.instance'].kind_of?(Admin::PartialsController)
+        end
+      end
+    end
+  end
+
   def current_user
     warden.user
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,7 @@ module ApplicationHelper
   def admin_navigation
     content_tag :header, id: 'adminNavigationOptions' do
       [
-        link_to(t('admin.navigation.blog'), root_path, id: 'linkHome'),
-        post_link,
+        menu.render,
         button_to(t("admin.navigation.logout"), session_path, method: :delete)
       ].join.html_safe
     end
@@ -12,20 +11,6 @@ module ApplicationHelper
   def title
     return @post.title unless @post.nil?
     return "pothibo's blog"
-  end
-
-  private
-
-  def post_link
-    html_class = %w(link)
-    if controller.kind_of? Admin::PostsController
-      html_class << "active"
-    end
-
-    [
-      link_to(t("admin.navigation.posts"), admin_posts_path, class: html_class),
-      link_to(t("admin.navigation.partials"), admin_partials_path, class: html_class)
-    ].join.html_safe
   end
 
 end
