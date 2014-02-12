ffcdb9dd5eb9f306334579aebb3aed87c7381ace
Pier-Olivier Thibault <pothibo@gmail.com>
2014-02-12 06:17:13 -0500

    Removed unused helpers to clean namespace
    Because helpers and decorators is customizable to the user' theme. It's
    best for Ecrire to limit the number of predefined helpers so user don't
    screw things up. Documentation should say that ApplicationHelper and
    OpenGraphHelper are protected namespace.
    
    Or maybe raise an exception if someone tries to use those namespace?

--- app/helpers/posts_helper.rb
+++
@@ -1,5 +0,0 @@
-module PostsHelper
-  def suggested_posts(current_post)
-    @posts ||= Post.published.limit(3).order("published_at DESC").without(current_post)
-  end
-end
--- app/helpers/splits_helper.rb
+++
@@ -1,2 +0,0 @@
-module SplitsHelper
-end
