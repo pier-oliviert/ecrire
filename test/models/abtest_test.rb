291fafffade542cbf7e35e1702f27d76c92c23ac
Pier-Olivier Thibault <pothibo@gmail.com>
2014-03-29 18:36:49 -0400

    Removed obsolete test and some comments

--- test/models/abtest_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AbtestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/partial_test.rb
+++ test/models/partial_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class PartialTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
