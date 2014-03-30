c6a1baee27294cefcab9b2569cd1f205e8825657
Pier-Olivier Thibault <pothibo@gmail.com>
2014-03-30 00:16:26 -0400

    Added more testing to the posts controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -18,6 +18,15 @@ class PostsControllerTest < BaseControllerTest
     assert_equal @controller.params[:page], page, "page param should be customized"
   end
 
+  test "redirect if the post isn't published" do
+    @post = posts(:draft)
+    get :show, year: 2003, month: 1, id: @post.id
+    assert_redirected_to :root
+  end
 
+  test "redirect if the post doesn't exist" do
+    get :show, year: 2003, month: 1, id: -1
+    assert_redirected_to :root
+  end
 
 end
