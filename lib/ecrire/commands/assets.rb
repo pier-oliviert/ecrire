f280bf7219be6da7c5c5b03636480ccb6514e5ce
Pier-Olivier Thibault <pothibo@gmail.com>
2014-05-15 10:07:05 -0400

    Removed method as it's the same as its ancestor

--- lib/ecrire/commands/assets.rb
+++ lib/ecrire/commands/assets.rb
@@ -11,14 +11,6 @@ module Ecrire
       @app.paths['public'].existent.first + @app.config.assets.prefix
     end
 
-    def manifest
-      if app
-        Sprockets::Manifest.new(index, output, app.config.assets.manifest)
-      else
-        super
-      end
-    end
-
     def compile!
       puts "Precompiling your assets to #{output}."
       with_logger do
