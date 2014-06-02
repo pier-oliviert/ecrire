ce5d720324aa3deca13c71b91082cb04b3ccc9c4
Pier-Olivier Thibault <pothibo@gmail.com>
2014-06-02 14:47:04 -0400

    edit_post_link now supports html options

--- lib/ecrire/app/helpers/posts_helper.rb
+++ lib/ecrire/app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
-  def edit_post_link
+  def edit_post_link(options = {})
     return unless signed_in?
 
-    link_to t('en.posts.edit'), edit_admin_post_path(post)
+    link_to t('posts.edit'), edit_admin_post_path(post), options
   end
 end
