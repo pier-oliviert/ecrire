5dfe165b157201c068cec2991ddbb95b7afe218e
Pier-Olivier Thibault <pothibo@gmail.com>
2014-06-10 13:15:49 -0400

    Moved post_path helper method to be accessible to the theme
    This way, theme can only call post_path(post) and ecrire will handle the
    transformation itself

--- lib/ecrire/app/helpers/posts_helper.rb
+++ lib/ecrire/app/helpers/posts_helper.rb
@@ -1,4 +1,16 @@
 module PostsHelper
+  def post_path(post, options = {})
+    if post.published?
+      options[:year] = post.published_at.year
+      options[:month] = post.published_at.month
+    else
+      options[:year] = post.created_at.year
+      options[:month] = post.created_at.month
+    end
+
+    super(post.becomes(::Post), options)
+  end
+
   def edit_post_link(options = {})
     return unless signed_in?
 
