36dc586b0fa69947eb0c3c8d69036164a0a62964
Pier-Olivier Thibault <pothibo@gmail.com>
2014-06-18 23:48:27 -0400

    Moved to H TO THE STORE
    This is a change in the migration where I removed HABTM and moved toward
    the Hstore data type with postgresql >= 9.2
    
    Now, the labels are saved in a key value store where the ids are joined
    in a string

--- lib/ecrire/db/migrate/20140618321984_create_meta_attributes.rb
+++
@@ -1,8 +0,0 @@
-class CreateMetaAttributes < ActiveRecord::Migration
-  def change
-    create_table :properties do |t|
-      t.references :post
-      t.references :data, polymorphic: true
-    end
-  end
-end
