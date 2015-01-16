d7475fffad75ea3627f5138797fd975408b623f3
Pier-Olivier Thibault <pothibo@gmail.com>
2015-01-16 11:11:35 -0500

    Fixed major issue when loading ecrire from production env.

--- lib/ecrire/railtie.rb
+++ lib/ecrire/railtie.rb
@@ -4,23 +4,6 @@ module Ecrire
     require 'ecrire/railtie/theme'
 
 
-    # This hack is done because ActiveRecord raise an error that makes
-    # Ecrire exit which makes it impossible to have an instance working without a
-    # database. By doing this, it becomes possible to Ecrire to load the server and
-    # serve the onboarding theme for the user.
-    ActiveRecord::Railtie.initializers.select do |initializer|
-      initializer.name.eql? 'active_record.initialize_database'
-    end.first.instance_variable_set :@block, Proc.new { |app|
-      ActiveSupport.on_load(:active_record) do
-        begin
-          establish_connection
-        rescue ActiveRecord::NoDatabaseError, ActiveRecord::AdapterNotSpecified
-          app.config.middleware.delete 'ActiveRecord::QueryCache'
-          app.config.middleware.delete 'ActiveRecord::ConnectionAdapters::ConnectionManagement'
-        end
-      end
-    }
-
     initializer 'ecrire.secrets', before: :bootstrap_hook do |app|
       app.paths.add 'config/secrets', with: Dir.pwd + '/secrets.yml'
     end
@@ -50,13 +33,13 @@ module Ecrire
       end
     end
 
+    Rails.application.paths.add 'config/database', with: Dir.pwd + '/secrets.yml'
+
     if Rails.env.production?
       include Ecrire::Railtie::Theme
     else
       begin
-        app = Rails.application
-        app.paths.add 'config/database', with: Dir.pwd + '/secrets.yml'
-        ActiveRecord::Base.configurations = app.config.database_configuration
+        ActiveRecord::Base.configurations = Rails.application.config.database_configuration
         ActiveRecord::Base.establish_connection
         sql = 'SELECT * from users limit(1)';
         user = ActiveRecord::Base.connection.execute(sql)
@@ -66,10 +49,28 @@ module Ecrire
           include Ecrire::Railtie::Onboarding
         end
       rescue Exception => e
-        app.config.active_record.migration_error = :none
+        Rails.application.config.active_record.migration_error = :none
         ActiveRecord::Base.configurations = {}
         include Ecrire::Railtie::Onboarding
       end
     end
+    
+    # This hack is done because ActiveRecord raise an error that makes
+    # Ecrire exit which makes it impossible to have an instance working without a
+    # database. By doing this, it becomes possible to Ecrire to load the server and
+    # serve the onboarding theme for the user.
+    ActiveRecord::Railtie.initializers.select do |initializer|
+      initializer.name.eql? 'active_record.initialize_database'
+    end.first.instance_variable_set :@block, Proc.new { |app|
+      ActiveSupport.on_load(:active_record) do
+        begin
+          establish_connection
+        rescue ActiveRecord::NoDatabaseError, ActiveRecord::AdapterNotSpecified => e
+          app.config.middleware.delete 'ActiveRecord::QueryCache'
+          app.config.middleware.delete 'ActiveRecord::ConnectionAdapters::ConnectionManagement'
+        end
+      end
+    }
+
   end
 end
