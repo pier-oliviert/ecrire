7fac5d361dd37b59e5855e1ce7804e7e940814fb
Pier-Olivier Thibault <pothibo@gmail.com>
2015-01-19 15:38:14 -0500

    PostsController can now be overriden by a theme

---
+++ lib/ecrire/app/controllers/ecrire/posts_controller.rb
@@ -0,0 +1,38 @@
+module Ecrire
+  class PostsController < ::ApplicationController
+    # TODO: This should be included. I have no idea why this isn't.
+    # It actually is loaded in dev as soon as the reloader reloads the files
+    include Rails.application.routes.url_helpers
+
+    before_action :pagination, only: :index
+    protect_from_forgery except: :index
+
+    helper_method :post
+
+    def index
+      respond_to do |format|
+        format.html
+        format.rss
+        format.json do
+          headers['Access-Control-Allow-Origin'] = '*'
+        end
+      end
+    end
+
+    def show
+      redirect_to :root and return if post.nil?
+      redirect_to :root and return unless post.published?
+    end
+
+    protected
+
+    def post
+      @post ||= Post.find_by_slug(params[:id])
+    end
+
+    def pagination
+      params[:per_page] ||= 10
+      params[:page] ||= 1
+    end
+  end
+end
