0b4fe1449ea9b221fcb266dcc4827cb011a843f8
Pier-Olivier Thibault <pothibo@gmail.com>
2015-03-05 19:50:57 -0500

    Fixed configuration of the PostBody element
    Trailing comma (I have no idea why it was there in the first place) was
    causing a problem with the attributes.

--- lib/ecrire/app/helpers/admin/posts_helper.rb
+++ lib/ecrire/app/helpers/admin/posts_helper.rb
@@ -10,9 +10,9 @@ module Admin
         href: admin_post_path(@post.id) do |div|
 
           if Rails.application.secrets.has_key?(:s3)
-            div[:bucket] = Rails.application.secrets.s3['bucket'],
-            div[:access_key] = Rails.application.secrets.s3['access_key'],
-            div[:signature] = image_form_signature(image_form_policy(@post)),
+            div[:bucket] = Rails.application.secrets.s3['bucket']
+            div[:access_key] = Rails.application.secrets.s3['access_key']
+            div[:signature] = image_form_signature(image_form_policy(@post))
             div[:policy] = image_form_policy(@post)
             if Rails.application.secrets.s3.has_key?('namespace')
               div['namespace'] = Rails.application.secrets.s3['namespace']
