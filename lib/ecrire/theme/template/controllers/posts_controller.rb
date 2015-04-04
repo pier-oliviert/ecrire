576e8085ac27ed5ef77263b1ea3ecfca40e57688
Pier-Olivier Thibault <pothibo@gmail.com>
2015-04-04 13:01:24 -0400

    Updating the default template so it's useable..

--- lib/ecrire/theme/template/controllers/posts_controller.rb
+++ lib/ecrire/theme/template/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < Ecrire::ThemeController
+  helper_method :post, :posts
+
+  def index
+    @posts = posts.published.includes(:titles).order('published_at DESC').page(params[:page]).per(params[:per])
+    @tags = Tag.all
+    super
+  end
+
+  def show
+    redirect_to :root and return if post.nil?
+    redirect_to :root and return unless post.published?
+    if post.titles.first != @title
+      redirect_to theme.post_path(post.year, post.month, post), status: :moved_permanently
+    end
+  end
+
+  protected
+
+  def posts
+    @posts ||= Post.published.page(params[:page]).per(params[:per]).order('published_at DESC')
+  end
+
+  def post
+    @title ||= Title.find_by_slug(params[:id])
+    @post ||= @title.post
+  end
+
 end
